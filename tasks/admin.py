693e0edea1c704dde7d43e442fa287d608face86
[lucadm] <[lucadimaio@gmail.com]>
2026-03-02 19:02:59 +0000

    Add task and category models on admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Task
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin): 
+    # Display the title, category, due date, and completion status in the admin list view
+    list_display = ['title', 'category', 'due_date', 'completed']
+    list_filter = ['category', 'completed', 'due_date']
+    search_fields = ['title']
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta: # This is to ensure that the plural form of "Category" is displayed correctly in the admin interface
+        verbose_name_plural = "Categories"
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    due_date = models.DateField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) # This creates a foreign key relationship to the Category model, allowing each task to be associated with a specific category. The on_delete=models.CASCADE argument ensures that if a category is deleted, all associated tasks will also be deleted.
+
+    def __str__(self):
+        return self.title
