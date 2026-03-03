146695e44c1d8d68b0f15d541e39a0087f82ac11
[lucadm] <[lucadimaio@gmail.com]>
2026-03-03 17:50:30 +0000

    Add forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Task
-
+from .forms import TaskForm
 # Create your views here.
 def task_list(request):
     #this view will display 2 tables, To do and Done, with tasks sorted by due date, with the most recent tasks at the top.
     todo_tasks = Task.objects.filter(completed=False).order_by('due_date')
     done_tasks = Task.objects.filter(completed=True).order_by('due_date')
-    return render(request, 'tasks/index.html', {'todo_tasks': todo_tasks, 'done_tasks': done_tasks})
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
 
+    context = {
+        'to_do_tasks': todo_tasks,
+        'done_tasks': done_tasks,
+        'form': form,
+    }
 
-def todo_tasks(request):
-    """View for displaying incomplete tasks (to do)"""
-    tasks = Task.objects.filter(completed=False).order_by('due_date')
-    return render(request, 'tasks/todo.html', {'tasks': tasks})
-
-
-def done_tasks(request):
-    """View for displaying completed tasks (done)"""
-    tasks = Task.objects.filter(completed=True).order_by('due_date')
-    return render(request, 'tasks/done.html', {'tasks': tasks})
+    return render(request, 'tasks/index.html', context)
+    
